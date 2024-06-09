47fdd0aef52e9b0551823b528e05861f1710fc54
Vishwa Vijetha G <vishwavijethag@gmail.com>
2024-06-10 01:25:25 +0530

    LeetCode Solutions

--- 3151_Special_Array_I.java
+++ 3151_Special_Array_I.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
+        
         int n = nums.length;
-        int prev = nums[0]%2==0 ? 0 : 1;
+
+        int prev = nums[0]%2;
+
         for(int i=1;i<n;i++){
-            if(prev == 0){
-                if(nums[i]%2==0)
-                    return false;
-                prev = 1;
-            }else{
-                if(nums[i]%2==1)
-                    return false;
-                prev = 0;
-            }
+            if(nums[i]%2==prev)
+                return false;
+            prev = nums[i]%2;
         }
+        
         return true;
     }
 }
