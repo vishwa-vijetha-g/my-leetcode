d760b1a47d7f5daf96b4b1afcd60bddd1e0a9ac7
Vishwa Vijetha G <vishwavijethag@gmail.com>
2024-06-21 22:26:45 +0530

    LeetCode Solutions

---
+++ 0134_Gas_Station.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        for(int i=0;i<gas.length;i++){
+            totalGas+=gas[i];
+            totalCost+=cost[i];
+        }
+
+        if(totalGas<totalCost) return -1;
+
+        int remainsGas = 0;
+        int start = 0;
+        
+        for(int i=0;i<gas.length;i++){
+            remainsGas+= (gas[i]-cost[i]);
+            if(remainsGas<0){
+                start = i+1;
+                remainsGas = 0;
+            }
+        }
+
+        return start;
+    }
+}
