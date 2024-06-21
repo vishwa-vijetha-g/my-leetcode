9b5c8861a60029627277d12cfd52204f5b177412
Vishwa Vijetha G <vishwavijethag@gmail.com>
2024-06-21 17:48:02 +0530

    Create 0287_Find_the_Duplicate_Number.java

---
+++ 0287_Find_the_Duplicate_Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        while(slow!=fast || (slow==0 && fast==0)){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        int slow2 = 0;
+
+        while(slow!=slow2){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+
+        return slow;
+    }
+}
